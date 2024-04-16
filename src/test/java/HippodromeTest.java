2db26a38e0470465d0626444250699157d919e7a
Hawkeye <A@b.c>
2024-04-16 17:37:18 +0300

    test 1.2

--- src/test/java/HippodromeTest.java
+++ src/test/java/HippodromeTest.java
@@ -1,2 +1,83 @@
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+
+import java.util.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
 public class HippodromeTest {
+
+
+    @Test
+    void constructorNullException() {
+        assertThrows(IllegalArgumentException.class, () -> new Hippodrome(null));
+    }
+
+    @Test
+    void constructorNullExceptionMsg() {
+        try {
+            new Hippodrome(null);
+        } catch (IllegalArgumentException e) {
+            assertEquals("Horses cannot be null.", e.getMessage());
+        }
+    }
+
+    @Test
+    void constructorEmptyList() {
+
+        assertThrows(IllegalArgumentException.class, () -> new Hippodrome(new ArrayList<>()));
+    }
+
+    @Test
+    void constructorEmptyListMsg() {
+        try {
+            new Hippodrome(new ArrayList<>());
+        } catch (IllegalArgumentException e) {
+            assertEquals("Horses cannot be empty.", e.getMessage());
+        }
+    }
+
+    @Test
+    void getHorses() {
+        ArrayList<Horse> horses = new ArrayList<>();
+        for (int i = 1; i < 31; i++) {
+            horses.add(new Horse(String.valueOf(i), i, i));
+        }
+        Hippodrome hippodrome = new Hippodrome(horses);
+
+        assertEquals(horses, hippodrome.getHorses());
+    }
+
+    @Disabled
+    @Test
+    public void getMoveTest() {
+        List<Horse> horses = new ArrayList<>();
+        for (int i = 0; i < 50; i++) {
+            horses.add(mock(Horse.class));
+        }
+
+        new Hippodrome(horses).move();
+
+        for (Horse horse : horses) {
+            verify(horse).move();
+        }
+    }
+
+    @Test
+    void getWinnerTest() {
+        Horse horse1 = new Horse("1", 4, 5);
+        Horse horse2 = new Horse("2", 4, 4);
+        Horse horse3 = new Horse("3", 4, 1);
+        Horse horse4 = new Horse("4", 4, 6);
+
+        Hippodrome hippodrome = new Hippodrome(List.of(horse1, horse2, horse3, horse4));
+
+        assertSame(horse4, hippodrome.getWinner() );
+
+    }
+
 }
+
