cb7ba0f22f593ac13fd1684f61016d9bedfd7cf6
Hawkeye <A@b.c>
2024-05-29 12:56:16 +0300

    Well done!

--- src/main/java/Hippodrome.java
+++ src/main/java/Hippodrome.java
@@ -1,5 +1,5 @@
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+
+import org.slf4j.LoggerFactory;
 
 import java.util.Collections;
 import java.util.Comparator;
@@ -8,7 +8,8 @@ import java.util.List;
 import static java.util.Objects.isNull;
 
 public class Hippodrome {
-    public static final Logger logger = LogManager.getLogger(Hippodrome.class);
+//   public static final Logger logger = LogManager.getLogger(Hippodrome.class);
+    public static final org.slf4j.Logger logger = LoggerFactory.getLogger(Main.class);
 
     private final List<Horse> horses;
 
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,16 @@
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class Main {
 
-   public static final Logger logger = LogManager.getLogger(Main.class);
+    public static final Logger logger = LoggerFactory.getLogger(Main.class);
 
+ //  public static final Logger logger = (Logger) LogManager.getLogger(Main.class);
 
     public static void main(String[] args) throws Exception {
 
